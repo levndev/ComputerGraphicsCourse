836a34334b0a369350d81cc42ca7ab94a877c5c0
Lev Nazarov <levndev@gmail.com>
2022-11-12 19:22:24 +0500

    доделал

--- Assignment5/Assignment5/Form1.cs
+++ Assignment5/Assignment5/Form1.cs
@@ -9,14 +9,24 @@ namespace Assignment5
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            pictureBox1.Image = Image.FromFile("tokyo.jpg");
+            var img = Image.FromFile("tokyo.jpg") as Bitmap;
+            for (var x = pictureBox1.Width / 2; x < pictureBox1.Width; x++)
+            {
+                for (var y = 0; y < img.Height; y++)
+                {
+                    var color = img.GetPixel(x, y);
+                    img.SetPixel(x, y, Color.FromArgb(color.R / 2, color.G / 2, color.B / 2));
+                }
+            }
+            pictureBox1.Image = img;
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
             var color = (pictureBox1.Image as Bitmap).GetPixel(e.Location.X, e.Location.Y);
             button1.BackColor = color;
-            label1.Text = (0.2126 * color.R + 0.7152 * color.G + 0.0722 * color.B).ToString("0.00");
+            var luminance = (0.2126 * color.R + 0.7152 * color.G + 0.0722 * color.B).ToString("0.00");
+            label1.Text = $"R:{color.R}, G:{color.G}, B:{color.B}, Luminance: {luminance}";
         }
     }
 }
